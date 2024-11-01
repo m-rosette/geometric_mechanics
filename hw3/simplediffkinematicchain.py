de100bfbec4c7ad2252934c5d17c42c002efe6bb
Marcus <rosettem@oregonstate.edu>
2024-11-01 13:26:51 -0700

    first pass at part2

--- hw3/simplediffkinematicchain.py
+++ hw3/simplediffkinematicchain.py
@@ -21,9 +21,11 @@ class DiffKinematicChain(kc.KinematicChain):
         """Initialize a kinematic chain augmented with Jacobian methods"""
 
         # Call the constructor for the base KinematicChain class
+        super().__init__(links, joint_axes) 
 
         # Create placeholders for the last-calculated Jacobian value and the index it was requested for
-
+        self.last_jacobian = None
+        self.jacobian_idx = -1 # starting with an invalid index as this is just a placeholder
 
     def Jacobian_Ad_inv(self,
                         link_index,  # Link number (with 1 as the first link)
@@ -33,6 +35,9 @@ class DiffKinematicChain(kc.KinematicChain):
 
         # Construct Jacobian matrix J as an ndarray of zeros with as many rows as the group has dimensions,
         # and as many columns as there are joints
+        num_rows = G.element_shape[0]
+        num_columns = len(self.joint_angles)
+        J = np.zeros((num_rows, num_columns))
 
         ########
         # Populate the Jacobian matrix by finding the transform from each joint before the chosen link to the
@@ -41,24 +46,35 @@ class DiffKinematicChain(kc.KinematicChain):
 
         # Make a list named link_positions_with_base that is the same as self.link_positions, but additionally has an
         # identity element inserted before the first entry
+        link_positions_with_base = np.insert(np.copy(self.link_positions), 0, G.identity_element(), axis=0)
 
         # Loop from 1 to link_index (i.e., range(1, link_index+1) )
         for j in range(1, link_index + 1):
 
             # create a transform g_rel that describes the position of the selected link relative the jth joint (which
             # is at the (j-1)th location in link_positions_with_base
+            g_rel = link_positions_with_base[j - 1]
 
             # use the Adjoint-inverse of this relative transformation to map the jth joint axis ( (j-1)th entry)
             # out to the end of the selected link in the link's body frame
+            J_joint = self.joint_axes[j - 1]
 
             # If the output_frame input is 'world', map the axis from the Lie algebra out to world coordinates
+            if output_frame == 'world':
+                J_joint = g_rel.Ad_inv(J_joint)
 
             # If the output_frame input is 'spatial', use the adjoint of the link position to map the axis back to
             # the identity
+            elif output_frame == 'spatial':
+                # Use the adjoint of the link position to map the axis back to the identity
+                J_joint = g_rel.Ad(J_joint)
 
             # Insert the value of J_joint into the (j-1)th index of J
+            J[:, j - 1] = J_joint.value[:num_rows]
 
             # Store J and the last requested index
+            self.last_jacobian = J.copy()
+            self.jacobian_idx = link_index
 
         return J
 
@@ -70,6 +86,9 @@ class DiffKinematicChain(kc.KinematicChain):
 
         # Construct Jacobian matrix J as an ndarray of zeros with as many rows as the group has dimensions,
         # and as many columns as there are joints
+        num_rows = G.element_shape[0]
+        num_columns = len(self.joint_angles)
+        J = np.zeros((num_rows, num_columns))
 
         ########
         # Populate the Jacobian matrix by finding the position of each joint in the world (which is the same as the
@@ -77,36 +96,76 @@ class DiffKinematicChain(kc.KinematicChain):
 
         # Make a list named link_positions_with_base that is the same as self.link_positions, but additionally has an
         # identity element inserted before the first entry
+        link_positions_with_base = np.insert(np.copy(self.link_positions), 0, G.identity_element(), axis=0)
 
         # Loop from 1 to link_index (i.e., range(1, link_index+1) )
         for j in range(1, link_index + 1):
 
             # use the Adjoint of the position of this joint to map its joint axis ( (j-1)th entry)
             # back to the identity of the group
+            J_joint = self.joint_axes[j - 1]
 
             # If the output_frame input is 'world', map the axis from the Lie algebra out to world coordinates
+            if output_frame == 'world':
+                J_joint = link_positions_with_base[j].Ad(J_joint)
 
             # If the output_frame input is 'body', use the adjoint-inverse of the link position to map the axis back to
             # the identity
+            elif output_frame == 'body':
+                J_joint = link_positions_with_base[j].Ad_inv(J_joint)
 
             # Insert the value of J_joint into the (j-1)th index of J
+            J[:, j - 1] = J_joint.value[:num_rows]
 
             # Store J and the last requested index
-
+            self.last_jacobian = J.copy()
+            self.jacobian_idx = link_index
 
         return J
 
-    def draw_Jacobian(self,
-                      ax):
-
+    def draw_Jacobian(self, ax):
         """ Draw the components of the last-requested Jacobian"""
 
-        # Get the location of the last-requested link, and use ut.column to make into a numpy column array
+        # Ensure that there is a last calculated Jacobian to draw
+        if self.last_jacobian is None or self.jacobian_idx < 1:
+            print("No Jacobian calculated. Please calculate the Jacobian first.")
+            return
+
+        # Get the location of the last-requested link
+        link_position = self.link_positions[self.jacobian_idx - 1]  # Get the position of the last requested link
+        link_end = link_position.value[:2]  # Extract the xy components of the link's end position
+
+        # Use ut.column to make the link_end into a numpy column array
+        link_end_column = ut.column(link_end)  # Ensure link_end is a column vector
 
         # Use np.tile to make a matrix in which each column is the coordinates of the link end
+        link_end_matrix = np.tile(link_end_column, (1, self.last_jacobian.shape[1]))
 
-        # Use ax.quiver to plot a set of arrows at the selected link end, (remembering to use only the xy components
-        # and not the theta component)
+        # Get the Jacobian components to draw arrows
+        jacobian_components = self.last_jacobian[:2, :]  # Get only the x and y components of the Jacobian
+
+        # Use ax.quiver to plot a set of arrows at the selected link end
+        ax.quiver(link_end_matrix[0, :], link_end_matrix[1, :],  # Starting points (x, y)
+                jacobian_components[0, :], jacobian_components[1, :],  # Direction (vx, vy)
+                angles='xy', scale_units='xy', scale=5, color='r')  # Customize arrow properties
+
+        # Set limits and labels for better visualization
+        # ax.set_xlim(-0.5, 5)
+        ax.set_ylim(-0.5, 3)
+        ax.set_xlabel('X-axis')
+        ax.set_ylabel('Y-axis')
+        ax.set_title('Jacobian at Link End')
+
+        # Set the plot aspect ratio to 'equal'
+        ax.set_aspect('equal')
+        ax.grid()
+
+def main(num_plots, links, joint_axes):
+    ax = plt.subplot(1, 3, figsize=(15, 5))
+
+    for i in range(num_plots):
+    
+        pass
 
 
 if __name__ == "__main__":
